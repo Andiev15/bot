bade21677e1a01933e60cece9e16579b2457d8c3
Andiev <oandiev@ozon.ru>
2021-12-01 20:38:57 +0300

    first iteration

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
+	tgbotapi "Token"
+)
+const token = "2105929017:AAHMP-ExS_S_Cne53CvPJP1pTxrE7XdkW4o"
 func main() {
-	fmt.Println("hello")
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: " + update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Andiev15/bot
 
 go 1.17
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.4.0-beta.0
